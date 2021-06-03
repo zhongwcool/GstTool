db324ca179c532813dc956eaaeb4decdcf6656bc
Alex zhong <zhongwcool@163.com>
2021-06-03 15:33:45 +0800

    how to stream a camera via gst

--- GstTool/MainWindow.xaml.cs
+++ GstTool/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Gst;
+using Gst.Video;
+using Debug = System.Diagnostics.Debug;
 
 namespace GstTool
 {
@@ -23,6 +13,92 @@ namespace GstTool
         public MainWindow()
         {
             InitializeComponent();
+            
+            Gst.Application.Init();
+            var mainLoop = new GLib.MainLoop();
+            System.Threading.ThreadPool.QueueUserWorkItem(x => mainLoop.Run());
+            
+            Loaded += (sender, args) =>
+            {
+                var pipeline = (Pipeline) Parse.Launch("udpsrc port=5004 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! decodebin ! textoverlay text=\"xx科技\" valignment=bottom halignment=left font-desc=\"微软雅黑, 22\" shaded-background=no ! timeoverlay halignment=left valignment=top ! d3dvideosink sync=false async=false");
+                //var pipeline = (Pipeline) Parse.Launch("videotestsrc pattern=0 ! videoconvert ! timeoverlay ! d3dvideosink");
+                //var pipeline = (Pipeline) Parse.Launch("playbin uri=http://stream.iqilu.com/vod_bag_2016//2020/02/16/903BE158056C44fcA9524B118A5BF230/903BE158056C44fcA9524B118A5BF230_H264_mp4_500K.mp4");
+
+                // subscribe to the messaging system of the bus and pipeline so we can monitor status as we go
+                // var bus = pipeline.Bus;
+                // bus.AddSignalWatch();
+                // bus.Message += Bus_Message;
+
+                // bus.EnableSyncMessageEmission();
+                // bus.SyncMessage += Bus_SyncMessage;
+                
+                var overlay= pipeline.GetByInterface(VideoOverlayAdapter.GType);
+                var adapter = new VideoOverlayAdapter(overlay.Handle)
+                {
+                    WindowHandle = VideoPanel.Handle
+                };
+                adapter.HandleEvents(true);
+
+                pipeline.SetState(State.Playing);
+            };
+        }
+        
+        private void Bus_SyncMessage(object o, SyncMessageArgs args)
+        {
+            if (Gst.Video.Global.IsVideoOverlayPrepareWindowHandleMessage(args.Message))
+            {
+                Debug.WriteLine("Bus_SyncMessage: Message prepare window handle received by: " + args.Message.Src.Name +
+                                " " + args.Message.Src.GetType());
+                Debug.WriteLine("");
+
+                try
+                {
+                    args.Message.Src["force-aspect-ratio"] = true;
+                }
+                catch (PropertyNotFoundException exception)
+                {
+                    Debug.WriteLine(exception);
+                }
+            }
+            else
+            {
+                args.Message.ParseInfo(out _, out var info);
+                Debug.WriteLine("Bus_SyncMessage: " + args.Message.Type + " - " + info);
+            }
+        }
+
+        private void Bus_Message(object o, MessageArgs args)
+        {
+            var msg = args.Message;
+            
+            switch (msg.Type)
+            {
+                case MessageType.Error:
+                    Debug.WriteLine("Bus_Message: Error received: " + msg);
+                    break;
+                case MessageType.StreamStatus:
+                    msg.ParseStreamStatus(out var status, out var theOwner);
+                    Debug.WriteLine("Bus_Message: 流状态: " + status + " ; Owner is: " +
+                                    theOwner.Name);
+                    break;
+                case MessageType.StateChanged:
+                    msg.ParseStateChanged(out var oldState, out var newState, out var pendingState);
+                    if (newState == State.Paused) args.RetVal = false;
+                    Debug.WriteLine("Bus_Message: 链路状态 from {0} to {1}: ; Pending: {2}",
+                        Element.StateGetName(oldState), 
+                        Element.StateGetName(newState),
+                        Element.StateGetName(pendingState)
+                        );
+                    break;
+                case MessageType.Element:
+                    Debug.WriteLine("Bus_Message: Element message: {0}", args.Message.ToString());
+                    break;
+                default:
+                    Debug.WriteLine("Bus_Message: 未处理: {0}", msg.Type);
+                    break;
+            }
+
+            args.RetVal = true;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # GstTool
 a tool of gst
+
+## How to use
+
+###  Server End
+
+stream a usb camera to your pc:
+```shell
+gst-launch-1.0 ksvideosrc device-name="设备列表中的相机设备名" ! videoconvert ! videoscale ! \ 
+video/x-raw,format=I420,width=1280,height=720,framerate=30/1 ! videoconvert ! \ 
+x264enc name=videoEnc bitrate=2000 tune=zerolatency pass=qual ! rtph264pay \
+! udpsink host=目标IP port=指定端口 sync=false -v
+```
+
+How to find your device-name:
+
+Take windows as an example:
+![](Art/dm.bmp)
+
+Now the "USB 2.0 Camera" is my device-name of personal camera. So I can run the following command on my pc:
+
+```shell
+gst-launch-1.0 ksvideosrc device-name="USB 2.0 Camera" ! videoconvert ! videoscale ! \
+video/x-raw,format=I420,width=1280,height=720,framerate=30/1 ! videoconvert ! \
+x264enc name=videoEnc bitrate=10000 tune=zerolatency pass=qual ! rtph264pay ! \
+udpsink host=192.168.5.152 port=5004 sync=false async=false
+```
+
+### User End
+
+Run the GstTool and you can see camera stream.
